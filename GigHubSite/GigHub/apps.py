a91e22f5338081a81eb24a1357d4dc2e48accaae
RusselDC <russel.delacruz.dc@bulsu.edu.ph>
2023-11-27 18:33:27 +0800

    push

---
+++ GigHubSite/GigHub/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GighubConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'GigHub'
---
+++ GigHubSite/GigHub/models.py
@@ -0,0 +1,95 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    USER_ROLES = [
+        ('A','Admin'),
+        ('JS','Job Seeker'),
+        ('JP','Job Provider')
+    ]
+    role = models.CharField(max_length=2, choices=USER_ROLES)
+    fName = models.CharField(max_length=255)
+    lName = models.CharField(max_length=255)
+    mName = models.CharField(max_length=255,null=True)
+    emailAddress = models.CharField(max_length=255)
+    contactNo = models.CharField(max_length=13)
+    civilStatus = models.CharField(max_length=20)
+    GENDER_CHOICES = [
+        ('M','Male'),
+        ('F','Female')
+    ]
+    sex = models.CharField(max_length=1,choices=GENDER_CHOICES)
+    birthDate = models.DateField('date_born')
+    houseNo = models.CharField(max_length=255, null=True)
+    street = models.CharField(max_length=255, null=True)
+    city = models.CharField(max_length=255, null=True)
+    province = models.CharField(max_length=255, null=True)
+
+    def __str__(self) -> str:
+        return self.fName
+
+
+
+class Company(models.Model):
+    employerID = models.ForeignKey(User, on_delete=models.CASCADE,limit_choices_to={'role': 'JP'})
+    companyName = models.CharField(max_length=255)
+    companyAddress = models.CharField(max_length=255)
+    companyEmail = models.CharField(max_length=255)
+    contactNumber = models.CharField(max_length=20)
+    isActive = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.companyName
+
+class JobPostings(models.Model):
+    companyID = models.ForeignKey(Company, on_delete=models.Case)
+    jobTitle = models.CharField(max_length=255) #input field
+    jobDescription = models.TextField() #text field na malaki
+    jobRequirements = models.CharField(max_length=255, blank=True, null=True) #multiple input fields na may same names like requirements[] para mareturn sila as array sa backend
+    jobLocation = models.CharField(max_length=255) # input field
+    salaryRange = models.CharField(max_length=255, blank=True, null=True) # dalawang input fields na may name na salary1 and salary2
+    deadLine = models.DateField("application_deadline")
+
+
+    def setJobRequirements(self, values):
+        self.jobRequirements = ','.join(map(str,values))
+
+    def getJobRequirements(self):
+        if self.jobRequirements:
+            return self.jobRequirements.split(',')
+        else:
+            return []
+        
+    def setSalary(self, values):
+        self.salaryRange = ','.join(map(str, values))
+
+    def getSalary(self):
+        if self.salaryRange:
+            return self.salaryRange.split(',')
+        else:
+            return []
+        
+
+        
+class JobApplication(models.Model):
+    applicantID = models.ForeignKey(User, on_delete=models.CASCADE)
+    jobID = models.ForeignKey(JobPostings, on_delete=models.CASCADE)
+    date = models.DateField("application_date")
+    STATUS_CHOICES = [
+        ('applied','Applied'),
+        ('in_progress','In Progress'),
+        ('rejected','Rejected'),
+        ('hired','Hired')
+    ]
+    applicationStatus = models.CharField(max_length=30, choices=STATUS_CHOICES)
+
+    
+
+    
+    
+
+       
+
+
+
