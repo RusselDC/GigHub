0a409178fe7aaff2759910e6935ddb6448a56dee
RusselDC <russel.delacruz.dc@bulsu.edu.ph>
2023-11-29 07:59:10 +0800

    forgotpassword di ko  natapos

--- GigHubSite/GigHub/views.py
+++ GigHubSite/GigHub/views.py
@@ -9,3 +9,9 @@ from django.urls import reverse
 def index(request):
     template = "index.html"
     return render(request, template)
+
+
+def login(request):
+    if request.method == "GET":
+        template = "login.html"
+        return render(request, template)
