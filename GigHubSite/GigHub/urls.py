5b6f37914ad0f90f7006da7e05fcb595c41eacb8
nieshxa <niknakriego@gmail.com>
2023-12-01 20:22:01 +0800

    register

--- GigHubSite/GigHub/urls.py
+++ GigHubSite/GigHub/urls.py
@@ -7,8 +7,10 @@ app_name = "GigHub"
 urlpatterns = [
     path('',views.index,name="index"),
     path('login/',views.login, name="login"),
+    path('register/',views.register, name="register"),
     path('forgot/',views.forgot, name="forgot"),
     path('verify/',views.verify, name="verify"),
     path('changePass/',views.changePass, name="changePass"),
-    path('logout/', views.logout, name="logout")
+    path('logout/', views.logout, name="logout"),
+    path('changeProfile/', views.Profilechange, name="changeProfile")
 ]
