b16ed2e418eeb6283416807fdc92d0fee2c432b8
RusselDC <russel.delacruz.dc@bulsu.edu.ph>
2023-12-11 17:09:44 +0800

    mamatay nakko

---
+++ GigHubSite/jobProvider/static/js/company.js
@@ -0,0 +1,391 @@
+function showValue(selectElement, input) {
+    var selectedText = selectElement.options[selectElement.selectedIndex].text;
+    input.value = selectedText;
+    selectElement.style.borderColor = "";
+    RemoveError(selectElement);
+  }
+  function Validate(form) {
+    const inputs = form.querySelectorAll("input[type='text']");
+    const selects = form.querySelectorAll("select");
+    const provinceInput = form.querySelector("#provinceInput"),
+      cityInput = form.querySelector("#cityInput"),
+      barangayInput = form.querySelector("#barangayInput"),
+      province = form.querySelector("#province"),
+      city = form.querySelector("#city"),
+      barangay = form.querySelector("#barangay");
+    isValid = true;
+  
+    var locationInput = [provinceInput, cityInput, barangayInput];
+    var location = [province, city, barangay];
+    locationInput.forEach((input, selectedIndex) => {
+      if (input.value === "") {
+        isValid = false;
+        ShowError(location[selectedIndex], "Field required");
+      } else {
+        RemoveError(location[selectedIndex]);
+      }
+    });
+    inputs.forEach((input) => {
+      if (input.value == "") {
+        isValid = false;
+        ShowError(input, "Field required");
+      } else {
+        RemoveError(input);
+      }
+    });
+    selects.forEach((select) => {
+      if (select.value == "") {
+        isValid = false;
+        ShowError(select, "Field required");
+      } else {
+        RemoveError(select);
+      }
+    });
+    if (isValid) {
+      Swal.fire({
+        title: "Add Company?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Confirm",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            text: "Company Added",
+            icon: "success",
+            showConfirmButton: false,
+          }).then(() => {
+            form.submit();
+          });
+        }
+      });
+    }
+  }
+  function Validate1(form) {
+    const inputs = form.querySelectorAll("input[type='text']");
+    const selects = form.querySelectorAll("select");
+    const provinceInput = form.querySelector("#provinceInput"),
+      cityInput = form.querySelector("#cityInput"),
+      barangayInput = form.querySelector("#barangayInput"),
+      province = form.querySelector("#provinceAdmin"),
+      city = form.querySelector("#cityAdmin"),
+      barangay = form.querySelector("#barangayAdmin");
+    isValid = true;
+  
+    var locationInput = [provinceInput, cityInput, barangayInput];
+    var location = [province, city, barangay];
+    locationInput.forEach((input, selectedIndex) => {
+      if (input.value === "") {
+        isValid = false;
+        ShowError(location[selectedIndex], "Field required");
+      } else {
+        RemoveError(location[selectedIndex]);
+      }
+    });
+    inputs.forEach((input) => {
+      if (input.value == "") {
+        isValid = false;
+        ShowError(input, "Field required");
+      } else {
+        RemoveError(input);
+      }
+    });
+    selects.forEach((select) => {
+      if (select.value == "") {
+        isValid = false;
+        ShowError(select, "Field required");
+      } else {
+        RemoveError(select);
+      }
+    });
+    if (isValid) {
+      Swal.fire({
+        title: "Add Company?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Confirm",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          $.ajax({
+            type:"POST",
+            url:'/provider/company/',
+            data:{'csrfmiddlewaretoken':token,'name':document.getElementById('name').value,'industry':document.getElementById('industry').value,'empRange':document.getElementById('employees').value,'designation':document.getElementById('designation').value,
+            'province':document.getElementById('provinceInput').value,'city':document.getElementById('cityInput').value,
+            'baranggay':document.getElementById('barangayInput').value,'street':document.getElementById('street').value,'bldg':document.getElementById('bldg').value},
+            success:(data)=>{
+              console.log(data)
+              if(data.status)
+              {
+                CloseForm(document.getElementById('forms'))
+                Swal.fire({
+                  text: "Company Added",
+                  icon: "success",
+                  showConfirmButton: false,
+                })
+                
+              }
+            },error:(err)=>{
+              console.log(err.responseText)
+            }
+          })
+          //Swal.fire({
+          //  text: "Company Added",
+          //  icon: "success",
+          //  showConfirmButton: false,
+          //}).then(() => {
+          //  form.submit();
+          //});
+        }
+      });
+    }
+  }
+  function Validate2(form) {
+    const inputs = form.querySelectorAll("input[type='text']");
+    const selects = form.querySelectorAll("select");
+    const provinceInput = form.querySelector("#provinceInputEdit"),
+      cityInput = form.querySelector("#cityInputEdit"),
+      barangayInput = form.querySelector("#barangayInputEdit"),
+      province = form.querySelector("#provinceEdit"),
+      city = form.querySelector("#cityEdit"),
+      barangay = form.querySelector("#barangayEdit");
+    isValid = true;
+  
+    var locationInput = [provinceInput, cityInput, barangayInput];
+    var location = [province, city, barangay];
+    locationInput.forEach((input, selectedIndex) => {
+      if (input.value === "") {
+        isValid = false;
+        ShowError(location[selectedIndex], "Field required");
+      } else {
+        RemoveError(location[selectedIndex]);
+      }
+    });
+    inputs.forEach((input) => {
+      if (input.value == "") {
+        isValid = false;
+        ShowError(input, "Field required");
+      } else {
+        RemoveError(input);
+      }
+    });
+    selects.forEach((select) => {
+      if (select.value == "") {
+        isValid = false;
+        ShowError(select, "Field required");
+      } else {
+        RemoveError(select);
+      }
+    });
+    if (isValid) {
+      Swal.fire({
+        title: "Add Company?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Confirm",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            text: "Company Added",
+            icon: "success",
+            showConfirmButton: false,
+          }).then(() => {
+            form.submit();
+          });
+        }
+      });
+    }
+  }
+  function ShowError(input, message) {
+    let parent = input.parentNode;
+    let errorMessage = parent.querySelector(".errorMessage");
+  
+    input.style.borderColor = "var(--red)";
+    errorMessage.innerText = message;
+  }
+  function RemoveError(input) {
+    let parent = input.parentNode;
+    let errorMessage = parent.querySelector(".errorMessage");
+  
+    input.style.borderColor = "";
+    errorMessage.innerText = "";
+  }
+  
+  function CloseForm(element) {
+    element.style.display = "none";
+  }
+  function ShowForm(element) {
+    element.style.display = "flex";
+    const inputs = element.querySelectorAll("input");
+    inputs.forEach((input) => {
+      input.value = "";
+      RemoveError(input);
+    });
+    const selects = element.querySelectorAll("select");
+    selects.forEach((input) => {
+      RemoveError(input);
+    });
+  }
+  
+  function BackButton() {
+    const companyContainer = document.querySelector("#companyContainer"),
+      companyDetails = document.querySelector("#companyDetails");
+    companyDetails.classList.remove("show");
+    companyContainer.classList.add("show");
+  }
+  
+  function ViewDetails(id) {
+    const companyContainer = document.querySelector("#companyContainer"),
+    companyDetails = document.querySelector("#companyDetails");
+    $.ajax({
+      type:"GET",
+      url:`/provider/getCompany/${id}/`,
+      success:(data)=>{
+        console.log(data)
+        document.getElementById('nameEdit').value = data.data.name
+
+        var selectElement = document.getElementById('industryEdit')
+        selectElement.options[0].value = data.data.industry
+        selectElement.options[0].text = data.data.industry
+        selectElement.options[0].selected = true
+        selectElement.options[0].disabled = false
+
+        var selectElement2 = document.getElementById('employeesEdit')
+        selectElement2.options[0].value = data.data.empRange
+        selectElement2.options[0].text = data.data.empRange
+        selectElement2.options[0].selected = true
+        selectElement2.options[0].disabled = false
+
+        var selectElement3 = document.getElementById('provinceEdit')
+        var newoption = document.createElement('option')
+        document.getElementById('provinceInputEdit').value = data.data.province
+        newoption.value = data.data.province
+        newoption.text = data.data.province
+        selectElement3.insertBefore(newoption,selectElement3.options[0])
+        newoption.selected = true;
+
+        var selectElement4 = document.getElementById('cityEdit')
+        var newoption2 = document.createElement('option')
+        document.getElementById('cityInputEdit').value = data.data.city
+        newoption2.value = data.data.city
+        newoption2.text = data.data.city
+        selectElement4.insertBefore(newoption2,selectElement4.options[0])
+        newoption2.selected = true;
+
+        var selectElement5 = document.getElementById('barangayEdit')
+        var newoption3 = document.createElement('option')
+        document.getElementById('barangayInputEdit').value = data.data.baranggay
+        newoption3.value = data.data.baranggay
+        newoption3.text = data.data.baranggay
+        selectElement5.insertBefore(newoption3,selectElement5.options[0])
+        newoption3.selected = true;
+        document.getElementById('streetEdit').value = data.data.strt
+        document.getElementById('bldgEdit').value = data.data.bldgNo
+
+        var selectElement6 = document.getElementById('designationEdit')
+        selectElement6.options[0].value = data.data.designation
+        selectElement6.options[0].text = data.data.designation
+        selectElement6.options[0].selected = true
+
+        document.getElementById('editbutton').addEventListener('click',()=>{
+          Validate2(formEdit,id)
+        })
+        
+        
+        
+        
+      },error:(err)=>{
+        console.log(err.responseText)
+      }
+    })
+    companyContainer.classList.remove("show");
+    companyDetails.classList.add("show");
+  }
+  const selects = form.querySelectorAll("select");
+  selects.forEach((select) => {
+    select.addEventListener("change", function () {
+      RemoveError(select);
+    });
+  });
+  const inputs = form.querySelectorAll("input");
+  inputs.forEach((input) => {
+    input.addEventListener("input", function () {
+      RemoveError(input);
+    });
+  });
+  
+  const formTitles = document.querySelectorAll(".formTitles"),
+  formContent = document.querySelectorAll(".companyDetailsSection");
+  
+  formTitles.forEach((forms, index) => {
+  forms.addEventListener("click", () => {
+    formContent.forEach((content) => {
+      content.classList.remove("active");
+    });
+  
+    formTitles.forEach((content) => {
+      content.classList.remove("active");
+    });
+  
+    formTitles[index].classList.add("active");
+    formContent[index].classList.add("active");
+  });
+  });
+
+
+  function Validate2(form, id) {
+   
+    document.getElementById('companyID').value = id
+    const inputs = form.querySelectorAll("input[type='text']");
+    const selects = form.querySelectorAll("select");
+    const provinceInput = form.querySelector("#provinceInputEdit"),
+      cityInput = form.querySelector("#cityInputEdit"),
+      barangayInput = form.querySelector("#barangayInputEdit"),
+      province = form.querySelector("#provinceEdit"),
+      city = form.querySelector("#cityEdit"),
+      barangay = form.querySelector("#barangayEdit");
+    isValid = true;
+  
+    var locationInput = [provinceInput, cityInput, barangayInput];
+    var location = [province, city, barangay];
+    locationInput.forEach((input, selectedIndex) => {
+      if (input.value === "") {
+        isValid = false;
+        ShowError(location[selectedIndex], "Field required");
+      } else {
+        RemoveError(location[selectedIndex]);
+      }
+    });
+    inputs.forEach((input) => {
+      if (input.value == "") {
+        isValid = false;
+        ShowError(input, "Field required");
+      } else {
+        RemoveError(input);
+      }
+    });
+    selects.forEach((select) => {
+      if (select.value == "") {
+        isValid = false;
+        ShowError(select, "Field required");
+      } else {
+        RemoveError(select);
+      }
+    });
+    if (isValid) {
+      Swal.fire({
+        title: "Add Company?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Confirm",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            text: "Company Added",
+            icon: "success",
+            showConfirmButton: false,
+          }).then(() => {
+            form.submit();
+          });
+        }
+      });
+    }
+  }
---
+++ GigHubSite/jobProvider/static/js/jobPostingClone.js
@@ -0,0 +1,310 @@
+function AcceptApplicant(id) {
+  Swal.fire({
+    title: "Hire Applicant",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Confirm",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      $.ajax({
+        type:"GET",
+        url:`/provider/hire/${id}`,
+        success:function(data){
+          if(data.status)
+          {
+            btn.style = "display:none"
+            let parentNode = btn.parentNode
+            parentNode.parentNode.querySelector('.statusTable').textContent = "Hired"
+            let acceptElement = parentNode.querySelector("#rejectBtn");
+            acceptElement.style = "display:''"
+            Swal.fire({
+              text: "Applicant Hired",
+              icon: "success",
+              showConfirmButton: false,
+            });
+          }
+        },error:function(err){
+          console.log(err.responseText)
+        }
+      })
+     
+    }
+  });
+}
+
+function MoveOnApplicant(btn,id)
+{
+  
+  Swal.fire({
+    title: "Accept Applicant",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Confirm",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      $.ajax({
+        type:"GET",
+        url:`/provider/accept/${id}`,
+        success:function(data){
+          if(data.status)
+          {
+            btn.style = "display:none"
+            let parentNode = btn.parentNode
+            let acceptElement = parentNode.querySelector("#accept");
+            acceptElement.style = "display:''"
+            parentNode.parentNode.querySelector('.statusTable').textContent = "In Progress"
+            Swal.fire({
+              text: "Applicant Accepted",
+              icon: "success",
+              showConfirmButton: false,
+            });
+          }
+        },error:function(err){
+          console.log(err.responseText)
+        }
+      })
+  
+    }
+  });
+}
+
+function RejectApplicant(btn,id) {
+  Swal.fire({
+    title: "Reject Applicant",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Confirm",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      $.ajax({
+        type:"GET",
+        url:`/provider/reject/${id}`,
+        success:function(data){
+          if(data.status)
+          {
+            btn.style = "display:none"
+            let parentNode = btn.parentNode
+            parentNode.parentNode.querySelector('.statusTable').textContent = "Rejected"
+            let acceptElement = parentNode.querySelector("#rejectBtn");
+              acceptElement.style = "display: none"
+              let progress = parentNode.querySelector("#onprogress");
+              progress.style = "display: none"
+              Swal.fire({
+                text: "Applicant Rejected",
+                icon: "success",
+                showConfirmButton: false,
+              });
+            }
+          },error:function(err){
+          console.log(err.responseText)
+        }
+      })
+    }
+  });
+}
+
+  
+  
+  const formTitles = document.querySelectorAll(".options"),
+    formContent = document.querySelectorAll(".container");
+  
+  formTitles.forEach((forms, index) => {
+    forms.addEventListener("click", () => {
+      formContent.forEach((content) => {
+        content.classList.remove("active");
+      });
+  
+      formTitles.forEach((content) => {
+        content.classList.remove("active");
+      });
+  
+      formTitles[index].classList.add("active");
+      formContent[index].classList.add("active");
+    });
+  });
+  function autoResizeTextarea(textarea) {
+    if (textarea.value == "") {
+      textarea.style.height = "";
+    } else {
+      textarea.style.height = "auto";
+      textarea.style.height = textarea.scrollHeight + "px";
+    }
+  }
+  
+  function ValidateReply(input, form) {
+    isValid = true;
+    if (input.value == "") {
+      isValid = false;
+      input.style.borderColor = "var(--red)";
+    }
+    if (isValid) {
+
+      $.ajax({
+        type:"POST",
+        url:"/provider/reply/",
+        data:$("#submitReply").serialize(),
+        success:(data)=>{
+          console.log(data)
+        },error:(err)=>{
+          console.log(err.responseText)
+        }
+      })
+      let template = document.querySelector('#providerReplyTemplate')
+      let templateContent = template.content.cloneNode(true)
+
+      templateContent.querySelector(".replyProviderContent").textContent = input.value
+      templateContent.querySelector("#providerDateTime").textContent = getCurrentDateTime();
+
+      document.querySelector(".commentContainer").appendChild(templateContent)
+
+
+    }
+  }
+  const textarea = document.querySelectorAll("textarea");
+  textarea.forEach((input) => {
+    input.addEventListener("input", () => {
+      input.style.borderColor = "";
+    });
+  });
+  
+  function ViewDetails(div,id) {
+    $.ajax({
+      type: "GET",
+      url: `/provider/getApplicant/${id}`,
+      success: (data) => {
+          console.log(data)
+          document.getElementById('postTitle').textContent = data.name
+          document.getElementById('name').textContent = data.name
+          document.getElementById('email').textContent = data.email
+          document.getElementById('number').textContent = data.contact
+          document.querySelector('#educationList').innerHTML = ""
+          document.querySelector('#experienceContainer').innerHTML = ""
+          document.querySelector('#awardList').innerHTML = ""
+          document.querySelector('#certificateList').innerHTML = ""
+          document.querySelector('.commentContainer').innerHTML = ""
+          data.colleges.forEach(element => {
+              let template = document.querySelector('#educationTemplate');
+              let templateContent = template.content.cloneNode(true);
+              console.log(templateContent.querySelector('#collegeName'))
+  
+              templateContent.querySelector('#collegeName').textContent = element.school[0]
+              templateContent.querySelector('#CourseName').textContent = element.course[0]
+              templateContent.querySelector('#majorName').textContent = element.award
+              templateContent.querySelector('#yearOf').textContent = `Class of ${element.year}`
+  
+              document.querySelector('#educationList').appendChild(templateContent);
+          })
+
+          data.employment.forEach(element => {
+            let template = document.querySelector('#employmentTemplate');
+            let templateContent = template.content.cloneNode(true);
+            let dutyList = templateContent.querySelector("#dutyList")
+
+            
+            templateContent.querySelector('.group1 span:nth-child(1)').textContent = element.name;
+            templateContent.querySelector('.group1 span:nth-child(2)').textContent = element.position;
+            templateContent.querySelector('.group2 span').textContent = `${element.start} - ${element.end}`;
+
+
+            element.duties.forEach(duty=>{
+              let listTemplate = document.querySelector('#duties')
+              let listtemplateContent = listTemplate.content.cloneNode(true);
+              
+              listtemplateContent.querySelector('#dutySpan').textContent = duty
+
+              dutyList.appendChild(listtemplateContent)
+
+            })  
+            document.querySelector('#experienceContainer').appendChild(templateContent);
+          });
+
+          data.awards.forEach(award=>{
+            let template = document.querySelector('#awardsTemplate')
+            let templateContent = template.content.cloneNode(true)
+
+            templateContent.querySelector("#awardName").textContent = award.title
+            templateContent.querySelector("#awardFrom").textContent = award.from
+            templateContent.querySelector("#awardDate").textContent = award.date
+
+            document.querySelector("#awardList").appendChild(templateContent)
+          })
+
+          data.certificates.forEach(award=>{
+            let template = document.querySelector('#awardsTemplate')
+            let templateContent = template.content.cloneNode(true)
+
+            templateContent.querySelector("#awardName").textContent = award.title
+            templateContent.querySelector("#awardFrom").textContent = award.from
+            templateContent.querySelector("#awardDate").textContent = award.date
+
+            document.querySelector("#certificateList").appendChild(templateContent)
+          })
+
+          document.getElementById('techSkills').textContent = data.skills.join(' | ')
+          document.getElementById('roomIDcontainer').value = data.room
+
+          data.messages.forEach(message=>{
+            if(message.role == "JP")
+            {
+              let template = document.querySelector('#providerReplyTemplate')
+              let templateContent = template.content.cloneNode(true)
+
+              templateContent.querySelector("#userImg").src = message.sender
+              templateContent.querySelector(".replyProviderContent").textContent = message.content
+              templateContent.querySelector("#providerDateTime").textContent = message.date
+
+              document.querySelector(".commentContainer").appendChild(templateContent)
+            }else{
+              let template = document.querySelector('#userReplyTemplate')
+              let templateContent = template.content.cloneNode(true)
+
+              templateContent.querySelector("#userImg").src = message.sender
+              templateContent.querySelector(".replyUserContent").textContent = message.content
+              templateContent.querySelector("#userDateTime").textContent = message.date
+
+              document.querySelector(".commentContainer").appendChild(templateContent)
+            }
+          })
+  
+      }, error: (err) => {
+          console.log(err.responseText)
+      }
+  })
+    div.style.display = "flex";
+  }
+  
+  function HideModal(event, el) {
+    var modal = document.getElementById("detailsContainer");
+  
+    if (event.target !== modal && !modal.contains(event.target)) {
+      el.style.display = "none";
+    }
+  }
+  
+  function BackButton() {
+    window.location.href = "/provider/jobs/";
+  }
+  
+  function getCurrentDateTime() {
+    const months = [
+      "January", "February", "March", "April",
+      "May", "June", "July", "August",
+      "September", "October", "November", "December"
+    ];
+  
+    const currentDate = new Date();
+  
+    const month = months[currentDate.getMonth()];
+    const day = currentDate.getDate();
+    const year = currentDate.getFullYear();
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+    const period = hours >= 12 ? 'PM' : 'AM';
+  
+    // Convert 24-hour format to 12-hour format
+    const formattedHours = hours % 12 || 12;
+  
+    const formattedDateTime = `${month} ${day}, ${year} ${formattedHours}:${minutes < 10 ? '0' : ''}${minutes} ${period}`;
+  
+    return formattedDateTime;
+  }
