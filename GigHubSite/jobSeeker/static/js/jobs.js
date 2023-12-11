9d4443f52ee0b3551ba6eea354e2d82cabb8b01b
nieshxa <niknakriego@gmail.com>
2023-12-11 12:08:57 +0800

    tinanggal ko na comments

--- GigHubSite/jobSeeker/static/js/jobs.js
+++ GigHubSite/jobSeeker/static/js/jobs.js
@@ -10,56 +10,15 @@ $(".view_job").click(function () {
 
 // change job btn txt on modal view
 $(".view_job").click(function () {
-  let id = $(this).attr("data-id");
-  alert(id);
+  let id = $(this).attr('data-id')
+  alert(id)
   var jobContainer = $(this).closest(".job_container");
   if (jobContainer.hasClass("modal_show")) {
     $(this).html("<iconify-icon icon='ic:round-close'></iconify-icon>");
-    $(".apply_btn").bind("click", () => {
+    $('.apply_btn').bind('click',()=>{
       //applyJob(id)
-    });
-    $(".view_jobdetails").addClass("currTab");
+    })
   } else {
     $(this).html("MORE");
-
-    $(".view_msg").removeClass("currTab");
-    $(".msg").css("display", "none");
-    $(".job_main").css("display", "block");
-    $(".job_details").css("display", "block");
-    $(".apply_btn").css("display", "none");
-    $(".job_unsaved").css("position", "static");
-    $(".job_saved").css("position", "static");
   }
-});
-$(document).ready(function () {
-  $(".view_jobdetails").addClass("currTab");
-  $(".msg").css("display", "none");
-
-  $(".view_msg").click(function () {
-    $(".tabJob").removeClass("currTab");
-    $(".view_msg").addClass("currTab");
-    $(this).addClass("currTab");
-
-    $(".msg").css("display", "flex");
-    $(".job_main").css("display", "none");
-    $(".job_details").css("display", "none");
-    $(".apply_btn").css("display", "none");
-    $(".job_saved").css("position", "absolute");
-    $(".job_saved").css("left", "100vw");
-    $(".job_unsaved").css("position", "absolute");
-    $(".job_unsaved").css("left", "100vw");
-  });
-
-  $(".view_jobdetails").click(function () {
-    $(".tabJob").removeClass("currTab");
-    $(".view_msg").removeClass("currTab");
-    $(this).addClass("currTab");
-
-    $(".msg").css("display", "none");
-    $(".job_main").css("display", "block");
-    $(".job_details").css("display", "block");
-    $(".apply_btn").css("display", "block");
-    $(".job_unsaved").css("position", "static");
-    $(".job_saved").css("position", "static");
-  });
 });
